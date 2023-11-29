e502ef71b7059dc7931484a11a2a9492227a3ac2
elmfer <elmfer11@outlook.com>
2023-11-28 21:58:09 -0600

    added navigation bar

---
+++ client/src/components/NavigationBar.jsx
@@ -0,0 +1,30 @@
+import SearchBar from './SearchBar';
+
+function NavigationBar(){
+  return (
+    <nav className="navbar bg-primary navbar-expand-md" data-bs-theme="dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">Fitness Tracker</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav-content" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="nav-content">
+          <ul className="navbar-nav me-auto">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Settings</a>
+            </li>
+          </ul>
+          <SearchBar/>
+          <button className="btn btn-dark">
+            <i class="fa-solid fa-moon"></i>
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default NavigationBar;
