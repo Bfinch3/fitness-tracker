c6c30405f5b09a20ebc74fbdf2e8cb235f426b83
elmfer <elmfer11@outlook.com>
2023-11-28 21:58:40 -0600

    added the workout listing stuff

---
+++ client/src/components/DropdownFilter.jsx
@@ -0,0 +1,15 @@
+function DropdownFilter({ text, isAdd }) {
+  return isAdd ? (
+    <button className="btn rounded-pill btn-primary btn-sm d-inline-flex gap-2 align-items-center">
+      Filter
+      <i className="fa fa-plus"></i>
+    </button>
+  ) : (
+    <button className="btn rounded-pill btn-primary btn-sm d-inline-flex gap-2 align-items-center">
+      { text }
+      <i className="fa fa-caret-down"></i>
+    </button>
+  );
+}
+
+export default DropdownFilter;
