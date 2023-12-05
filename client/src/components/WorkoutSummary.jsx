7b9ae83ed3ed3fc3b252c469929ff04ac436263d
elmfer <elmfer10@gmail.com>
2023-12-05 15:16:27 -0600

    remove text underlining

--- client/src/components/WorkoutSummary.jsx
+++ client/src/components/WorkoutSummary.jsx
@@ -1,9 +1,13 @@
 // these are the summary workout cards that appear on the workoutlist
 
+const style = {
+  textDecoration: "none"
+};
+
 //summary needs to include type, title, and notes
 function WorkoutSummary({ title, description, tags }) {
   return (
-    <a href="">
+    <a href="" style={style}>
     <div className="card workout-item">
       <div className="card-body">
         <h5 className="card-title d-flex flex-wrap gap-2">
