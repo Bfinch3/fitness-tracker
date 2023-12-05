15e014664707a3220e7547e4c2e589d7026db531
elmfer <elmfer10@gmail.com>
2023-12-05 11:40:30 -0600

    userpage

--- client/src/pages/Userpage.jsx
+++ client/src/pages/Userpage.jsx
@@ -2,15 +2,15 @@ import Aside from '../components/Aside'
 import WorkoutList from '../components/WorkoutList'
 
 const mainDivStyle = {
-  margin: "0 auto",
-  maxWidth: "1200px"
-};
+    margin: "0 auto",
+    maxWidth: "1200px"
+  };
 
 export default function UserPage() {
-  return (
-    <div className="d-flex gap-2 p-2 align-items-start flex-wrap" style={ mainDivStyle }>
-      <WorkoutList/>
-      <Aside/>
-    </div>
-  );
-}
+    return (
+        <div className="d-flex gap-2 p-2 align-items-start flex-wrap" style={ mainDivStyle }>
+        <WorkoutList/>
+        <Aside/>
+</div>
+    );
+  }
