6650bdb2d9f0b0803928bc28c49839150cf1aa45
Billy Finch <billyrfinch@gmail.com>
2023-12-05 10:51:50 -0600

    route changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const db = require("./config/connection");
+const db = require("./server/config/connection");
 const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
