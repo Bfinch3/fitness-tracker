1e0b5ce1edfb99ad0c66410c1ed460dcd26dfcb9
clvalada <clvalada@gmail.com>
2023-12-06 13:58:02 -0500

    updated Navigation Bar

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -1,25 +1,24 @@
-import SearchBar from './SearchBar';
+import React from 'react';
 
-function NavigationBar(){
+function NavigationBar() {
   return (
     <nav className="navbar bg-primary navbar-expand-md" data-bs-theme="dark">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Fitness Tracker</a>
+        <a className="navbar-brand" href="/">Fitness Tracker</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav-content" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="nav-content">
           <ul className="navbar-nav me-auto">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
+              <a className="nav-link" href="/">Home</a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">Settings</a>
             </li>
           </ul>
-          <SearchBar/>
           <button className="btn btn-dark">
-            <i class="fa-solid fa-moon"></i>
+            <i className="fa-solid fa-moon"></i>
           </button>
         </div>
       </div>
