31ef9d1628d2a86c7df6819952fb9f9dd5bfc2e4
lindseycarlson23 <144490541+lindseycarlson23@users.noreply.github.com>
2023-12-06 11:04:29 -0600

    added modallaunch to userpage

--- client/src/pages/UserPage.jsx
+++ client/src/pages/UserPage.jsx
@@ -1,5 +1,6 @@
-import Aside from '../components/Aside'
-import WorkoutList from '../components/WorkoutList'
+import Aside from '../components/Aside';
+import WorkoutList from '../components/WorkoutList';
+import ModalLaunch from '../components/ModalLaunch';
 
 const mainDivStyle = {
     margin: "0 auto",
@@ -9,6 +10,7 @@ const mainDivStyle = {
 export default function UserPage() {
     return (
         <div className="d-flex gap-2 p-2 align-items-start flex-wrap" style={ mainDivStyle }>
+        <ModalLaunch/>
         <WorkoutList/>
         <Aside/>
 </div>
