c1bd41b0d2c800eb69d17f18d05e56e2132d5139
elmfer <elmfer10@gmail.com>
2023-12-06 15:50:37 -0600

    added styling to homepage and use 'AuthCarousel'

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,15 +1,38 @@
-import LoginForm from "../components/LoginForm";
-import Signup from "../components/Signup";
+import { useEffect } from "react";
+import AuthCarousel from "../components/AuthCarousel";
 
+const titleStyle = {
+  fontSize: "6rem",
+  zIndex: "5",
+  fontWeight: "",
+  textShadow: "0 0 40px rgba(0, 0, 0, 0.5)",
+  fontFamily: "'Noto Sans', sans-serif"
+};
 
-export default function Homepage() {
-    return (
-        
-      <div>       
-          <LoginForm />
-          <Signup />
-      </div>
-    );
+function formatMainDiv() {
+  const mainDiv = document.getElementById("main-div");
+  mainDiv.classList.remove("container");
 }
 
-//add in styling, graphic, logo
+export default function Homepage() {
+
+  useEffect(formatMainDiv, []);
+
+  return (
+        
+    <div id="homepage-main-hero">       
+      <div className="d-flex py-3 md-py-3 gap-3 align-items-center xs-flex-column container">
+        <div className="flex-grow-1 d-grid">
+          <div className="layer xs-text-center">
+            <img src="assets/images/logo/512.png" className="brand-image"/>
+          </div>
+          <div className="text-white layer d-flex flex-column-reverse brand-title">
+            <div className="">Fitness Tracker</div>
+          </div>
+        </div>
+        {/* The AuthCarousel has both the login and signup forms that can be switched by clicking thier respective tabs */}
+        <AuthCarousel />
+      </div>
+    </div>
+  );
+}
