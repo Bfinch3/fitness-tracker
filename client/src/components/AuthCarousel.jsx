94b4efbc613f3ccd8e0a2c5742cb144a8eda3738
elmfer <elmfer10@gmail.com>
2023-12-06 16:34:05 -0600

    made auth form carousel tranisition more compatible with other browsers

--- client/src/components/AuthCarousel.jsx
+++ client/src/components/AuthCarousel.jsx
@@ -10,23 +10,23 @@ function AuthCarousel() {
   }, [authForm]);
 
   const formTransition = {
-    translate: authForm == "login" ? "0%" : "-110%"
+    transform: authForm == "login" ? "translate(0%, 0%)" : "translate(-110%, 0%)"
   };
 
   return (
     <div className="d-flex flex-column align-items-center gap-1 auth-form-size">
       <ul className="nav rounded-pill overflow-hidden bg-light-subtle nav-pills nav-fill w-100 box-shadow">
         <li className="nav-item">
-          <button onClick={() => setAuthForm("login")} className={`nav-link rounded-0 ${authForm == "login" ? 'active' : ''}`}>Login</button>
+          <button onClick={() => { setAuthForm("login") }} className={`nav-link rounded-0 ${authForm == "login" ? 'active' : ''}`}>Login</button>
         </li>
         <li className="nav-item">
-          <button onClick={() => setAuthForm("signup")} className={`nav-link rounded-0 ${authForm == "signup" ? 'active' : ''}`}>Signup</button>
+          <button onClick={() => { setAuthForm("signup") }} className={`nav-link rounded-0 ${authForm == "signup" ? 'active' : ''}`}>Signup</button>
         </li>
       </ul>
       <div className="overflow-hidden p-2 md-w-100">
         <div className="d-grid auth-carousel" style={formTransition}>
           <div className="layer"><LoginForm/></div>
-          <div className="layer" style={{translate: "110% 0%"}}><Signup /></div>
+          <div className="layer" style={{transform: "translate(110%, 0%)"}}><Signup /></div>
         </div>
       </div>
     </div>
