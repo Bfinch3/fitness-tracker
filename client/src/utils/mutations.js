2f936c49313681347d729d10ae873388ddf1f5c9
lindseycarlson23 <144490541+lindseycarlson23@users.noreply.github.com>
2023-12-06 13:50:51 -0600

    updated ADD_USER

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,45 +1,45 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
-mutation login($email: String!, $password: String!) {
-  login(email: $email, password: $password) {
-    token
-    profile {
-      _id
-      name
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      profile {
+        _id
+        username
+      }
     }
   }
-}
 `;
 
 export const ADD_USER = gql`
-mutation login($username: String! $email: String!, $password: String!) {
-  login(username: $username, email: $email, password: $password) {
-    token
-    user {
-      _id
-      username
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
   }
-}
 `;
 
 export const ADD_WORKOUT = gql`
-mutation addWorkout($memeberId: ID!, $workout: String!) {
-  addWorkout(memberId: $memberId, workout: $workout) {
-    _id
-    name
-    workouts
+  mutation addWorkout($memeberId: ID!, $workout: String!) {
+    addWorkout(memberId: $memberId, workout: $workout) {
+      _id
+      name
+      workouts
+    }
   }
-}
 `;
 
 export const REMOVE_WORKOUT = gql`
-mutation removeWorkout($workout: String!) {
-  removeWorkout(workout: $workout) {
-    _id
-    name
-    workouts
+  mutation removeWorkout($workout: String!) {
+    removeWorkout(workout: $workout) {
+      _id
+      name
+      workouts
+    }
   }
-}
 `;
