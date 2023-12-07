713d163586b0f18b0de3803ec56efae1d74572a4
lindseycarlson23 <144490541+lindseycarlson23@users.noreply.github.com>
2023-12-07 13:05:08 -0600

    more updates to workoutpage

--- client/src/pages/WorkoutPage.jsx
+++ client/src/pages/WorkoutPage.jsx
@@ -3,47 +3,26 @@
 import { useQuery } from "@apollo/client";
 import Workout from "../components/Workout";
 import { useParams } from "react-router-dom";
-import CommentsSection from "../components/CommentsSection";
+// import CommentsSection from "../components/CommentsSection";
+import CommentList from "../components/CommentList";
+import CommentForm from "../components/CommentForm";
 import { Card } from "react-bootstrap";
 
-// import { QUERY_WORKOUT } from "../utils/queries";
+import { QUERY_WORKOUT } from "../utils/queries";
 
 const SingleWorkoutPage = () => {
-  // const { workoutId } = useParams();
+  const { workoutId } = useParams();
 
-  // const { loading, data } = useQuery(QUERY_WORKOUT, {
-  //   variables: { workoutId },
-  // });
+  const { loading, data } = useQuery(QUERY_WORKOUT, {
+    variables: { workoutId },
+  });
 
-  // const workout = data?.workout || {};
+  const workout = data?.workout || {};
 
-  // if (loading) {
-  //   return <div>Loading...</div>;
-  // }
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-  const workoutListData = [
-    {
-        _id: 1, 
-        Type: "Yoga",
-        Title: "30 min Yoga Flow",
-        Url: "https://members.onepeloton.com/classes/yoga?modal=classDetailsModal&classId=5b0c7818d280400aaa4a83b1d0ea0c74",
-        Notes: "I only did the first half, but there was a flowing section that was like dancing that I loved. A lot of the movements were fluid. Also, the music was celebrating the 75th anniversary of Atlantic Records."
-    },
-    {
-      _id: 2,
-        Type: "Meditation",
-        Title: "10 min Courage Meditation",
-        Url: "https://members.onepeloton.com/classes/meditation?modal=classDetailsModal&classId=02d7567767b54bb9ab2027325180c10e",
-        Notes: "Included oceanic breathing and the mini-self avatar entering through your third eye and sitting on a golden throne in your heart."
-    },
-    {
-        _id: 3, 
-        Type: "Cycling",
-        Title: "30 min 90s Rock Ride",
-        Url: "https://members.onepeloton.com/classes/cycling?modal=classDetailsModal&classId=5e133499066d40fd85f14d318ab9373f",
-        Notes: "Great playlist including Pearl Jam, Third Eye Blind, Metallica, Counting Crows, Better Than Ezra, Smashing Pumpkins!"
-    }
-]
 
 
 
@@ -53,7 +32,7 @@ const SingleWorkoutPage = () => {
     //   <Workout/>
     // </div>
   <Card>
-    <Card.Title>{Title}</Card.Title>
+    <Card.Title>{workout.workoutTitle}</Card.Title>
     <div className="justify-self-end d-flex gap-2">
             <button className="btn btn-primary">
               <i className="fa-solid fa-pencil"></i>
@@ -63,10 +42,15 @@ const SingleWorkoutPage = () => {
             </button>
           </div>
     <Card.Body>
-      <Card.Text>{workout.}</Card.Text>
-      <Card.Text>Workout URL</Card.Text>
-      <Card.Text>Workout Notes</Card.Text>
-      <Card.Text>{CommentsSection}</Card.Text>
+      <Card.Text>{workout.workoutType}</Card.Text>
+      <Card.Text>{workout.workoutUrl}</Card.Text>
+      <Card.Text>{workout.workoutText}</Card.Text>
+      <div className="my-5">
+        <CommentList comments={thought.comments} />
+      </div>
+      <div className="m-3 p-4" style={{ border: '1px dotted #1a1a1a' }}>
+        <CommentForm thoughtId={thought._id} />
+      </div>
     </Card.Body>
   </Card>
 
