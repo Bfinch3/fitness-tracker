f69b53f50237b4587e6c1ceaa9f08267f3b34756
elmfer <elmfer11@outlook.com>
2023-12-07 23:59:27 -0600

    made the toolbar it's own component

--- client/src/components/MarkdownEditor.jsx
+++ client/src/components/MarkdownEditor.jsx
@@ -1,131 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-
-const TextAreaUtils = {
-  getCursors: function(textarea) {
-    let cursorStart = textarea.selectionStart;
-    let cursorEnd = textarea.selectionEnd;
-
-    return { cursorStart, cursorEnd };
-  },
-
-  spliceInSyntax: function(textarea, prefix, suffix) {
-    let { cursorStart, cursorEnd } = this.getCursors(textarea);
-    let text = textarea.value;
-    let selectedText = text.substring(cursorStart, cursorEnd);
-
-    if(!suffix) {
-      suffix = prefix;
-    }
-
-    let newText = text.substring(0, cursorStart) + prefix + selectedText + suffix + text.substring(cursorEnd);
-
-    textarea.value = newText;
-    textarea.selectionStart = cursorStart + prefix.length;
-    textarea.selectionEnd = cursorEnd + prefix.length;
-  },
-  appendInSyntax: function(textarea, prefix, suffix) {
-    let text = textarea.value;
-    
-    if(!suffix) {
-      text += `\n${prefix}`;
-      textarea.value = text;
-      return;
-    }
-
-    let newText = `\n${prefix}${suffix}`;
-    textarea.value += newText;
-
-    textarea.selectionStart = textarea.value.length - suffix.length;
-    textarea.selectionEnd = textarea.value.length - suffix.length;
-  },
-  doesSelectionHaveSyntax: function(textarea, prefix, suffix) {
-    let { cursorStart, cursorEnd } = this.getCursors(textarea);
-    let text = textarea.value;
-    let selectedText = text.substring(cursorStart, cursorEnd);
-
-    if(!suffix) {
-      suffix = prefix;
-    }
-
-    return selectedText.startsWith(prefix) && selectedText.endsWith(suffix);
-  }
-};
-
-const ToolbarItems = {
-  heading: {
-    name: "Heading",
-    icon: "fa-h",
-    onClick: function(textarea) {
-      TextAreaUtils.appendInSyntax(textarea, '###');
-    }
-  },
-  bold: {
-    name: "Bold",
-    icon: "fa-bold",
-    onClick: function(textarea) {
-      if(!TextAreaUtils.doesSelectionHaveSyntax(textarea, '**')) {
-        TextAreaUtils.spliceInSyntax(textarea, '**');
-      }
-    }
-  },
-  italic: {
-    name: "Italic",
-    icon: "fa-italic",
-    onClick: function(textarea) {
-      if(!TextAreaUtils.doesSelectionHaveSyntax(textarea, '_')) {
-        TextAreaUtils.spliceInSyntax(textarea, '_');
-      }
-    }
-  },
-  quote: {
-    name: "Quote",
-    icon: "fa-quote-right",
-    onClick: function(textarea) {
-      TextAreaUtils.appendInSyntax(textarea, '>');
-    }
-  },
-  code: {
-    name: "Code",
-    icon: "fa-code",
-    onClick: function(textarea) {
-      if(!TextAreaUtils.doesSelectionHaveSyntax(textarea, '`')) {
-        TextAreaUtils.spliceInSyntax(textarea, '`', '`');
-      }
-    }
-  },
-  link: {
-    name: "Link",
-    icon: "fa-link",
-    onClick: function(textarea) {
-      if(!TextAreaUtils.doesSelectionHaveSyntax(textarea, '[', '](url)')) {
-        TextAreaUtils.spliceInSyntax(textarea, '[', '](url)');
-      }
-    }
-  },
-  list: {
-    name: "List",
-    icon: "fa-list-ul",
-    onClick: function(textarea) {
-      TextAreaUtils.appendInSyntax(textarea, '* ');
-    }
-  },
-  numberedList: {
-    name: "Numbered List",
-    icon: "fa-list-ol",
-    onClick: function(textarea) {
-      TextAreaUtils.appendInSyntax(textarea, '1. ');
-    }
-  },
-  image: {
-    name: "Image",
-    icon: "fa-image",
-    onClick: function(textarea) {
-      if(!TextAreaUtils.doesSelectionHaveSyntax(textarea, '![', '](url)')) {
-        TextAreaUtils.spliceInSyntax(textarea, '![', '](url)');
-      }
-    }
-  }
-};
+import MarkdownEditorToolbar from "./MarkdownEditorToolbar";
 
 function MarkdownEditor({ text, setText }) {
   const [mode, setMode] = useState("write");
@@ -143,27 +17,6 @@ function MarkdownEditor({ text, setText }) {
     }
   }, [text, mode]);
 
-  function Toolbar() {
-    return (
-      <>
-        {
-          Object.values(ToolbarItems).map((item) => {
-            return (
-              <button key={item.name} className="btn btn-link btn-sm" onMouseDown={(e) => {
-                e.preventDefault();
-                e.stopPropagation();
-                item.onClick(textArea.current);
-                setEditText(textArea.current.value);
-              }}>
-                <i className={`fa-solid ${item.icon}`}></i>
-              </button>
-            );
-          })
-        }
-      </>
-    );
-  }
-
   return (
     <div className="d-flex flex-column gap-3">
       <div>
@@ -176,7 +29,7 @@ function MarkdownEditor({ text, setText }) {
           </li>
         </ul>
           <div className={mode != "write" ? "d-none" : ""}>
-            <Toolbar />
+            <MarkdownEditorToolbar textArea={textArea} setText={setText} />
             <textarea className="form-control" spellCheck="false" ref={textArea} style={{height: "4in"}} onInput={() => setText(textArea.current.value)}></textarea>
           </div>
           <div ref={markdownPreview} className={`markdown-view py-3 ${mode == "write" ? "d-none" : ""}`}></div>
