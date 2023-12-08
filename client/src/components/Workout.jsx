a62b23ad755304b8f3c30d8ff36e5bb529cebb37
elmfer <elmfer11@outlook.com>
2023-12-08 02:41:39 -0600

    made workout editable

--- client/src/components/Workout.jsx
+++ client/src/components/Workout.jsx
@@ -2,47 +2,141 @@
 
 import WorkoutLinks from "./WorkoutLinks";
 import CommentsSection from "./CommentsSection";
+import MarkdownEditor from "./MarkdownEditor";
+import { Form } from "react-bootstrap";
+
+import { useEffect, useRef, useState } from "react";
 
 const profilePictureStyle = {
   height: "0.5in",
   borderRadius: "0.25in"
 };
 
-function Workout({ title, type, links, comments }) {
+function Workout({ title, type, text, links, comments }) {
+  const [inEditMode, setEditMode] = useState(false);
+  const [isSaving, setIsSaving] = useState(false);
 
+  // These variables are for when the user edits the workout, separate from the actual workout content
+  const [editTitle, setEditTitle] = useState(title);
+  const [editType, setEditType] = useState(type);
+  const [editText, setEditText] = useState(text);
+  const [editLinks, setEditLinks] = useState([...links]); 
+
+  const titleInputField = useRef();
+  const workoutTypeSelection = useRef();
+  const textDiv = useRef();
+
+  const markdownConverter = new showdown.Converter();
+
+  useEffect(() => {
+    titleInputField.current.value = editTitle;
+    workoutTypeSelection.current.value = editType;
+    textDiv.current.innerHTML = markdownConverter.makeHtml(editText);
+  }, []);
+
+  function resetEdits() {
+    setEditTitle(title);
+    setEditType(type);
+    setEditText(text);
+    setEditLinks(links);
+    titleInputField.current.value = title;
+    workoutTypeSelection.current.value = type;
+  }
   
+  // ------ NOTE FOR BACKEND --------
+  // This is the function is where we implement saving the edits to the server.
+  // This function is called when the user wants to save their edits.
+  async function onSave() {
+    setIsSaving(true);
+
+    // ---- These variables are what the user edited and used for sending a mutate query to the backend -----//
+    // ---- If the user hasn't edited, for example, the title, it will be the original -----//
+    // ---- If the user has edited, for example, the text, it will be the edited text ------//
+    // editTitle;
+    // editType;
+    // editText;
+    // editLinks;
+
+    // TODO: Send query to update workout by workoutId
+
+    // TODO: Wait for the query
+
+    // Reload the page
+    window.location.reload();
+  }
+
+  function renderEditButtons() {
+    const btnWidth = {
+      width: "2.6rem"
+    };
+
+    return !inEditMode ? (
+      <>
+        <button className="btn btn-primary" style={btnWidth} onClick={() => setEditMode(true)}>
+          <i className="fa-solid fa-pencil"></i>
+        </button>
+        <button className="btn btn-danger" style={btnWidth}>
+          <i className="fa-solid fa-trash"></i>
+        </button>
+      </>
+    ) : (
+      <>
+        <button className="btn btn-success" style={btnWidth} onClick={onSave}>
+        {
+          !isSaving ? (<i className="fa-solid fa-floppy-disk"></i>) :
+          (<span class="spinner-border spinner-border-sm" aria-hidden="true"/>)
+        }
+        </button>
+        <button className="btn btn-danger" style={btnWidth} onClick={() => {setEditMode(false); resetEdits();}}>
+          <i className="fa-solid fa-ban"></i>
+        </button>
+      </>
+    );
+  }
+
   return (
     <div className="card flex-grow-1 box-shadow col-7">
       <div className="card-header">
         <h4 className="card-title d-flex gap-2 align-items-center mb-0">
           <img style={profilePictureStyle} src="/default-pfp.jpg"></img>
-          <span>{ title }</span>
+          
+          <span className={inEditMode ? "d-none" : ""}>{ title }</span>
+          <input ref={titleInputField} className={!inEditMode ? "d-none" : "form-control"} onInput={() => setEditTitle(titleInputField.current.value)}/>
 
           <span className="flex-grow-1"></span>
 
           <div className="justify-self-end d-flex gap-2">
-            <button className="btn btn-primary">
-              <i className="fa-solid fa-pencil"></i>
-            </button>
-            <button className="btn btn-danger">
-              <i className="fa-solid fa-trash"></i>
-            </button>
+            { renderEditButtons() }
           </div>
         </h4>
 
         <div className="d-flex gap-2 align-items-center pt-2">
           <span>Summary</span>
           <i className="fa-solid fa-caret-right"></i>
-          <span className="badge bg-secondary rounded-pill p-2 d-inline-flex gap-2">
+          <span className={`badge bg-secondary rounded-pill p-2 d-inline-flex gap-2 ${inEditMode ? "d-none" : ""}`}>
             <i className="fa-solid fa-bookmark"></i>
             {type}
           </span>
+          <Form.Select className={!inEditMode ? "d-none" : ""} ref={workoutTypeSelection} size="sm" onChange={() => setEditType(workoutTypeSelection.current.value)}>
+            <option>Strength</option>
+            <option>Meditation</option>
+            <option>Yoga</option>
+            <option>Cardio</option>
+            <option>Cycling</option>
+            <option>Outdoor</option>
+            <option>Running</option>
+            <option>Walking</option> 
+            <option>Stretching</option> 
+          </Form.Select>
         </div>
       </div>
       <div className="card-body">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, eos. Iusto eligendi accusamus iste culpa reprehenderit ab fuga est aliquam molestiae optio eos hic enim, corrupti placeat nemo voluptatem vel.</p>
+        <div className={`markdown-view py-3 ${inEditMode ? 'd-none' : ''}`} ref={textDiv}/>
+        <div className={!inEditMode ? 'd-none' : ''}>
+          <MarkdownEditor text={editText} setText={setEditText} />
+        </div>
 
-        <WorkoutLinks links={links}/>
+        <WorkoutLinks inEditMode={inEditMode} links={links} editLinks={editLinks} setEditLinks={setEditLinks}/>
 
         <hr/>
 
--- client/src/components/WorkoutLinks.jsx
+++ client/src/components/WorkoutLinks.jsx
@@ -1,13 +1,136 @@
 // added to Workout.jsx, this is where the url will go
+import { useEffect, useRef, useState } from "react";
+import { Modal } from "react-bootstrap";
 
 const linkStyle = {
-  zIndex: 5,
+  zIndex: 15,
   boxShadow: "0 0 5px var(--darker)"
 };
 
-function WorkoutLinks({links}) {
+function AddLinkModal({setIndex, editLinks, setEditLinks}) {
+  const textInput = useRef();
+  const urlInput = useRef();
 
-  if(!links) return;
+  function onSubmit(e) {
+    e.preventDefault();
+    const editedLinks = [...editLinks];
+    editedLinks.push({ text: textInput.current.value, url: urlInput.current.value });
+    setEditLinks(editedLinks);
+  }
+
+  return (
+    <div className="modal fade" id="add-link-modal">
+    <div className="modal-dialog">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h1 className="modal-title fs-5">Add Link</h1>
+        <button type="button" className="btn-close" data-bs-dismiss="modal"/>
+      </div>
+      <form onSubmit={onSubmit}>
+        <div className="modal-body">
+          <div className="mb-3">
+            <label htmlFor="edit-link-text" className="form-label">Text</label>
+            <input ref={textInput} type="text" className="form-control" id="edit-link-text" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="edit-link-url" className="form-label">URL</label>
+            <input ref={urlInput} type="url" className="form-control" id="edit-link-url" />
+          </div>
+        </div>
+        <div className="modal-footer">
+          <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={() => setIndex(-1)}>Close</button>
+          <button type="submit" className="btn btn-primary" data-bs-dismiss="modal">Save changes</button>
+        </div>
+      </form> 
+    </div>
+    </div>
+    </div>
+  );
+}
+
+function EditLinkModal({index, setIndex, editLinks, setEditLinks}) {
+  const textInput = useRef();
+  const urlInput = useRef();
+
+  useEffect(() => {
+    if(index < 0) return;
+
+    textInput.current.value = editLinks[index].text;
+    urlInput.current.value = editLinks[index].url;
+  }, [index]);
+
+  function onSubmit(e) {
+    e.preventDefault();
+    const editedLinks = [...editLinks];
+    editedLinks[index] = { text: textInput.current.value, url: urlInput.current.value };
+    setEditLinks(editedLinks);
+  }
+
+  return (
+    <div className="modal fade" id="edit-link-modal">
+    <div className="modal-dialog">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h1 className="modal-title fs-5">Edit Link</h1>
+        <button type="button" className="btn-close" data-bs-dismiss="modal"/>
+      </div>
+      <form onSubmit={onSubmit}>
+        <div className="modal-body">
+          <div className="mb-3">
+            <label htmlFor="edit-link-text" className="form-label">Text</label>
+            <input ref={textInput} type="text" className="form-control" id="edit-link-text" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="edit-link-url" className="form-label">URL</label>
+            <input ref={urlInput} type="url" className="form-control" id="edit-link-url" />
+          </div>
+        </div>
+        <div className="modal-footer">
+          <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={() => setIndex(-1)}>Close</button>
+          <button type="submit" className="btn btn-primary" data-bs-dismiss="modal">Save changes</button>
+        </div>
+      </form> 
+    </div>
+    </div>
+    </div>
+  );
+}
+
+function WorkoutLinks({inEditMode, links, editLinks, setEditLinks}) {
+  const [linkIndex, setLinkIndex] = useState(-1);
+
+  if(!links && !inEditMode) return;
+
+  if(inEditMode) return (
+    <>
+      <h4>Edit Links</h4>
+
+      <div className="d-flex flex-column gap-2">
+        { editLinks.map((link, index) => (
+          <div className="input-group overflow-hidden" key={link.url}>
+            <button className="btn btn-secondary" style={linkStyle} onClick={() =>  setLinkIndex(index) } data-bs-toggle="modal" data-bs-target="#edit-link-modal">
+              <i className="fa fa-pencil"/>
+            </button>
+            <button className="btn btn-primary text-start flex-grow-1" onClick={() => setLinkIndex(index)} data-bs-toggle="modal" data-bs-target="#edit-link-modal">
+              { link.text }
+            </button>
+            <button className="btn btn-danger" style={linkStyle} onClick={() => { const editedLinks = [...editLinks]; editedLinks.splice(index, 1); setEditLinks(editedLinks); }}>
+              <i className="fa fa-trash"/>
+            </button>
+          </div>
+          ))
+        }
+        <div className="d-flex justify-content-end">
+          <button className="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#add-link-modal">
+            <i className="fa fa-plus"/>
+          </button>
+        </div>
+      </div>
+
+      <EditLinkModal index={linkIndex} setIndex={setLinkIndex} editLinks={editLinks} setEditLinks={setEditLinks} />
+      <AddLinkModal setIndex={setLinkIndex} editLinks={editLinks} setEditLinks={setEditLinks} />
+    </>
+  );
 
   return (
     <>
