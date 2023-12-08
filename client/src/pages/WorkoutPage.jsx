e53d12bb138c456ce87faac9de075fe5578a3419
lindseycarlson23 <144490541+lindseycarlson23@users.noreply.github.com>
2023-12-08 09:59:48 -0600

    minor changes to workoutpage

--- client/src/pages/WorkoutPage.jsx
+++ client/src/pages/WorkoutPage.jsx
@@ -23,36 +23,39 @@ const SingleWorkoutPage = () => {
     return <div>Loading...</div>;
   }
 
-
+  const mainDivStyle = {
+    margin: "0 auto",
+    maxWidth: "1200px"
+  };
 
 
 
   return (
-    // <div className="d-flex gap-2 p-2 align-items-start flex-wrap" style={ mainDivStyle }>
-    //   <Workout/>
-    // </div>
-  <Card>
-    <Card.Title>{workout.workoutTitle}</Card.Title>
-    <div className="justify-self-end d-flex gap-2">
-            <button className="btn btn-primary">
-              <i className="fa-solid fa-pencil"></i>
-            </button>
-            <button className="btn btn-danger">
-              <i className="fa-solid fa-trash"></i>
-            </button>
-          </div>
-    <Card.Body>
-      <Card.Text>{workout.workoutType}</Card.Text>
-      <Card.Text>{workout.workoutUrl}</Card.Text>
-      <Card.Text>{workout.workoutText}</Card.Text>
-      <div className="my-5">
-        <CommentList comments={thought.comments} />
-      </div>
-      <div className="m-3 p-4" style={{ border: '1px dotted #1a1a1a' }}>
-        <CommentForm thoughtId={thought._id} />
-      </div>
-    </Card.Body>
-  </Card>
+    <div className="d-flex gap-2 p-2 align-items-start flex-wrap" style={ mainDivStyle }>
+      <Workout/>
+    </div>
+  // <Card>
+  //   <Card.Title>{workout.workoutTitle}</Card.Title>
+  //   {/* <div className="justify-self-end d-flex gap-2">
+  //           <button className="btn btn-primary">
+  //             <i className="fa-solid fa-pencil"></i>
+  //           </button>
+  //           <button className="btn btn-danger">
+  //             <i className="fa-solid fa-trash"></i>
+  //           </button>
+  //         </div> */}
+  //   <Card.Body>
+  //     <Card.Text>{workout.workoutType}</Card.Text>
+  //     <Card.Text>{workout.workoutUrl}</Card.Text>
+  //     <Card.Text>{workout.workoutText}</Card.Text>
+  //     {/* <div className="my-5">
+  //       <CommentList comments={thought.comments} />
+  //     </div>
+  //     <div className="m-3 p-4" style={{ border: '1px dotted #1a1a1a' }}>
+  //       <CommentForm thoughtId={thought._id} />
+  //     </div> */}
+  //   </Card.Body>
+  // </Card>
 
   )
 }
