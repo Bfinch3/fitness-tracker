255616e834270f26530f6c870b1f80c4a1b846b2
elmfer <elmfer11@outlook.com>
2023-12-09 18:49:51 -0600

    made workout editable

--- client/src/components/Workout.jsx
+++ client/src/components/Workout.jsx
@@ -1,48 +1,148 @@
 // this feeds WorkoutPage
 
-import WorkoutLinks from "./WorkoutLinks";
 import CommentsSection from "./CommentsSection";
-import WorkoutList from "./WorkoutList";
+import MarkdownEditor from "./MarkdownEditor";
+import WorkoutLink from "./WorkoutLink";
+import { Form } from "react-bootstrap";
+import { useState, useRef, useEffect } from "react";
 
 const profilePictureStyle = {
   height: "0.5in",
   borderRadius: "0.25in"
 };
 
-function Workout({ workoutType, title, url, notes, comments }) {
+function Workout({ type, title, link, notes, comments }) {
+  const [inEditMode, setEditMode] = useState(false);
+  const [isSaving, setIsSaving] = useState(false);
+
+  // These variables are for when the user edits the workout, separate from the actual workout content
+  const [editTitle, setEditTitle] = useState(title);
+  const [editType, setEditType] = useState(type);
+  const [editNotes, setEditNotes] = useState(notes);
+  const [editLink, setEditLink] = useState(link); 
+
+  const titleInputField = useRef();
+  const workoutTypeSelection = useRef();
+  const textDiv = useRef();
+
+  const markdownConverter = new showdown.Converter();
+
+  useEffect(() => {
+    titleInputField.current.value = title;
+    workoutTypeSelection.current.value = type;
+    textDiv.current.innerHTML = markdownConverter.makeHtml(notes);
+  }, []);
+
+  function resetEdits() {
+    setEditTitle(title);
+    setEditType(type);
+    setEditNotes(notes);
+    setEditLink(link);
+    titleInputField.current.value = title;
+    workoutTypeSelection.current.value = type;
+  }
+
+  // ------ NOTE FOR BACKEND --------
+  // This is the function is where we implement saving the edits to the server.
+  // This function is called when the user wants to save their edits.
+  async function onSave() {
+    setIsSaving(true);
+
+    // ---- These variables are what the user edited and used for sending a mutate query to the backend -----//
+    // ---- If the user hasn't edited, for example, the title, it will be the original -----//
+    // ---- If the user has edited, for example, the notes, it will be the edited note ------//
+    editTitle;
+    editType;
+    editNotes;
+    editLink;
+
+    // TODO: Send query to update workout by workoutId
+
+    // TODO: Wait for the query
+
+    // Reload the page
+    window.location.reload();
+  }
+
+  function renderEditButtons() {
+    const btnWidth = {
+      width: "2.6rem"
+    };
+
+    return !inEditMode ? (
+      <>
+        <button className="btn btn-primary" style={btnWidth} onClick={() => setEditMode(true)}>
+          <i className="fa-solid fa-pencil"></i>
+        </button>
+        <button className="btn btn-danger" style={btnWidth}>
+          <i className="fa-solid fa-trash"></i>
+        </button>
+      </>
+    ) : (
+      <>
+        <button className="btn btn-success" style={btnWidth} onClick={onSave}>
+        {
+          !isSaving ? (<i className="fa-solid fa-floppy-disk"></i>) :
+          (<span class="spinner-border spinner-border-sm" aria-hidden="true"/>)
+        }
+        </button>
+        <button className="btn btn-danger" style={btnWidth} onClick={() => {setEditMode(false); resetEdits();}}>
+          <i className="fa-solid fa-ban"></i>
+        </button>
+      </>
+    );
+  }
+
   return (
     <div className="card flex-grow-1 box-shadow col-7">
       <div className="card-header">
         <h4 className="card-title d-flex gap-2 align-items-center mb-0">
           <img style={profilePictureStyle} src="/default-pfp.jpg"></img>
-          <span>{ title }</span>
+          
+          <span className={inEditMode ? "d-none" : ""}>{ title }</span>
+          <input ref={titleInputField} className={!inEditMode ? "d-none" : "form-control"} onInput={() => setEditTitle(titleInputField.current.value)}/>
 
           <span className="flex-grow-1"></span>
 
           <div className="justify-self-end d-flex gap-2">
-            <button className="btn btn-primary">
-              <i className="fa-solid fa-pencil"></i>
-            </button>
-            <button className="btn btn-danger">
-              <i className="fa-solid fa-trash"></i>
-            </button>
+            { renderEditButtons() }
           </div>
         </h4>
 
         <div className="d-flex gap-2 align-items-center pt-2">
-          {workoutType}
+          <span>Summary</span>
+          <i className="fa-solid fa-caret-right"></i>
+          <span className={`badge bg-secondary rounded-pill p-2 d-inline-flex gap-2 ${inEditMode ? "d-none" : ""}`}>
+            <i className="fa-solid fa-bookmark"></i>
+            {type}
+          </span>
+          <Form.Select className={!inEditMode ? "d-none" : ""} ref={workoutTypeSelection} size="sm" onChange={() => setEditType(workoutTypeSelection.current.value)}>
+            <option>Strength</option>
+            <option>Meditation</option>
+            <option>Yoga</option>
+            <option>Cardio</option>
+            <option>Cycling</option>
+            <option>Outdoor</option>
+            <option>Running</option>
+            <option>Walking</option> 
+            <option>Stretching</option> 
+          </Form.Select>
         </div>
       </div>
       <div className="card-body">
-        <p>{title}</p>
-        { url }
+        <div className={`markdown-view py-3 ${inEditMode ? 'd-none' : ''}`} ref={textDiv}/>
+        <div className={!inEditMode ? 'd-none' : ''}>
+          <MarkdownEditor text={editNotes} setText={setEditNotes} />
+        </div>
+
+        <WorkoutLink inEditMode={inEditMode} link={link} editLink={editLink} setEditLink={setEditLink}/>
 
         <hr/>
 
         <CommentsSection comments={comments} />
       </div>
     </div>
-  );
+  )
 }
 
 export default Workout;
