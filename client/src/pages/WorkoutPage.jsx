2d08720c7b45bfcfefd25e1986a40c7f67ee857e
elmfer <elmfer11@outlook.com>
2023-12-09 18:48:48 -0600

    send link to workout card

--- client/src/pages/WorkoutPage.jsx
+++ client/src/pages/WorkoutPage.jsx
@@ -3,12 +3,11 @@
 import { useQuery } from "@apollo/client";
 import Workout from "../components/Workout";
 import { useParams } from "react-router-dom";
-// import CommentsSection from "../components/CommentsSection";
-import CommentList from "../components/CommentList";
-import CommentForm from "../components/CommentForm";
-import { Card } from "react-bootstrap";
-import ModalLaunch from "../components/ModalLaunch";
-import { Link } from "react-router-dom";
+// import CommentList from "../components/CommentList";
+// import CommentForm from "../components/CommentForm";
+// import { Card } from "react-bootstrap";
+// import ModalLaunch from "../components/ModalLaunch";
+// import { Link } from "react-router-dom";
 
 import { QUERY_WORKOUT } from "../utils/queries";
 
@@ -32,9 +31,15 @@ console.log(data);
 
   console.log(workout);
 
+  // NOTE: The backend should at one point return the workout object containg a link field with the following format:
+  const link = {
+    text: workout.url, // The text is shown in place of the url
+    url: workout.url
+  }
+
   return (
     <div className="d-flex gap-2 p-2 align-items-start flex-wrap" style={ mainDivStyle }>
-      <Workout title={workout.workoutTitle} type={workout.workoutType} text={workout.workoutText} url={workout.url} comments={workout.comments} />
+      <Workout title={workout.workoutTitle} type={workout.workoutType} text={workout.workoutText} link={link} comments={workout.comments} />
     </div>
   // <Card>
   //   <Card.Title>{workout.workoutTitle}</Card.Title>
