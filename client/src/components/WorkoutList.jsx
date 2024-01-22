6f448ee281e2fcfaddbdb6f8c36c4641062241f8
elmfrain <elmfer10@gmail.com>
2024-01-21 20:12:26 -0600

    be able to view workouts by type

--- client/src/components/WorkoutList.jsx
+++ client/src/components/WorkoutList.jsx
@@ -5,20 +5,25 @@ import { QUERY_WORKOUTS } from "../utils/queries";
 import { useQuery } from "@apollo/client";
 
 import Auth from "../utils/auth";
+import { useEffect, useState } from "react";
 
 function WorkoutList() {
 
- const { loading, data } = useQuery(QUERY_WORKOUTS, {
-  variables: { userId: Auth.getProfile().data._id },
-  
-});
+  const [ filterType, setFilterType ] = useState(null);
+
+  const { loading, data, refetch } = useQuery(QUERY_WORKOUTS, { variables: { userId: Auth.getProfile().data._id } });
+
+  useEffect(() => {
+    refetch({type: filterType})
+    console.log(filterType);
+  }, [filterType]);
 
   return (
     <div className="card flex-grow-1 box-shadow col-7 md-order-2 lg-w-100">
       <div className="card-header d-flex align-items-center">
         <h4 className="card-title d-flex flex-wrap gap-2 mb-0 mt-1">
           <span>My Workouts</span>
-          <DropdownFilter />
+          <DropdownFilter setFilterType={setFilterType} />
         </h4>
         <div className="flex-grow-1" />
         <ModalLaunch />
