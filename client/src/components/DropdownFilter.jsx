2fdcb4fbd736dbda75f0c9cfe77124200ee0d64d
elmfrain <elmfer10@gmail.com>
2024-01-21 20:12:07 -0600

    be able to set filter type for WorkoutList

--- client/src/components/DropdownFilter.jsx
+++ client/src/components/DropdownFilter.jsx
@@ -1,18 +1,24 @@
 
 
-function DropdownFilter({children}) {
+function DropdownFilter({ setFilterType }) {
+
+  function onChange(e) {
+    const filterType = e.target.value != "none" ? e.target.value : null;
+    setFilterType(filterType);
+  }
+
   return (
-    <select className="btn rounded-pill btn-primary btn-sm d-inline-flex gap-2 align-items-center">
-      <option>Filter by Type</option>
-      <option>Strength</option>
-      <option>Meditation</option>
-      <option>Yoga</option>
-      <option>Cardio</option>
-      <option>Cycling</option>
-      <option>Outdoor</option>
-      <option>Running</option>
-      <option>Walking</option> 
-      <option>Stretching</option> 
+    <select onChange={onChange} className="btn rounded-pill btn-primary btn-sm d-inline-flex gap-2 align-items-center px-2">
+      <option value="none">Filter by Type</option>
+      <option value="Strength">Strength</option>
+      <option value="Meditation">Meditation</option>
+      <option value="Yoga">Yoga</option>
+      <option value="Cardio">Cardio</option>
+      <option value="Cycling">Cycling</option>
+      <option value="Outdoor">Outdoor</option>
+      <option value="Running">Running</option>
+      <option value="Walking">Walking</option> 
+      <option value="Stretching">Stretching</option> 
     </select>
   );
 }
